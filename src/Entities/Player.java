15b58667c0b47524edbe65d4084c4ef61cabe0de
alecg <alec.g.c15@gmail.com>
2020-03-04 22:36:12 -0800

    Added movement to work with arrow keys.

--- src/Entities/Player.java
+++ src/Entities/Player.java
@@ -19,11 +19,12 @@ public class Player extends Mob{
     public void tick() {
         //if(KeyInput.isDown(KeyEvent.VK_W)) jump(10);
         // if(KeyInput.isDown(KeyEvent.VK_S)) dy = 5;
-        if(KeyInput.isDown(KeyEvent.VK_D)) dx = 5;
-        if(KeyInput.isDown(KeyEvent.VK_A)) dx = -5;
+        if(KeyInput.isDown(KeyEvent.VK_D) || KeyInput.isDown(KeyEvent.VK_RIGHT)) dx = 5;
+        if(KeyInput.isDown(KeyEvent.VK_A) || KeyInput.isDown(KeyEvent.VK_LEFT)) dx = -5;
 
         // if(KeyInput.wasReleased(KeyEvent.VK_W) || KeyInput.wasReleased(KeyEvent.VK_S)) dy = 0;
-        if(KeyInput.wasReleased(KeyEvent.VK_A) || KeyInput.wasReleased(KeyEvent.VK_D)) dx = 0;
+        if(KeyInput.wasReleased(KeyEvent.VK_A) || KeyInput.wasReleased(KeyEvent.VK_D) ||
+                (KeyInput.wasReleased(KeyEvent.VK_RIGHT) || KeyInput.wasReleased(KeyEvent.VK_LEFT))) dx = 0;
         if(x <= 0 - sprite.getWidth() / 2)
             x = Game.WIDTH - sprite.getWidth() / 2;
         if(x > Game.WIDTH - sprite.getWidth() / 2)
